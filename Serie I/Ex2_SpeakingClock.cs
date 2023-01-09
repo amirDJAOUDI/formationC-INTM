830717128651b3205cbf27776319cd81d86b7f6e
amir djaoudi <amirouche.djaoudi@yahoo.com>
2023-01-09 17:17:07 +0100

    exercice 2

--- Serie I/Ex2_SpeakingClock.cs
+++ Serie I/Ex2_SpeakingClock.cs
@@ -10,8 +10,26 @@ namespace Serie_I
     {
         public static string GoodDay(int heure)
         {
-            //TODO
-            return string.Empty;
+            string affichage;
+
+            if (heure >= 0 && heure <= 23) { 
+
+                if (heure <= 6 )
+                    { affichage = "Merveilleuse nuit !"; }
+                else if (heure < 12)
+                    { affichage = "Bonne matinée !"; } 
+                else if (heure == 12)
+                    { affichage = "Bon appétit !"; }
+                else if (heure >= 18)
+                    { affichage = "Profitez de votre après-midi !"; }
+
+                else 
+                    { affichage = "Passez une bonne soirée !"; }
+
+
+            } else { affichage = "La valeur tapée n'est pas dans la plage horraire de 0 à 23H"; }
+
+            return affichage;
         }
     }
 }

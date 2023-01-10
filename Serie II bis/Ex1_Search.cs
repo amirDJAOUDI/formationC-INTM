756e15e00f047f6f83691b09bdff8226d80812d8
amir djaoudi <amirouche.djaoudi@yahoo.com>
2023-01-10 17:16:08 +0100

    serie II excercie 1

---
+++ Serie II bis/Ex1_Search.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    public class Search
+    {
+        public int LinearSearch(int[] tableau, int valeur)
+        {
+            int indice = -1;
+
+            for (int i = 0; i < tableau.Length; i++)
+            {
+
+                if (valeur == tableau[i])
+                {
+                    indice = i;
+                };
+            }
+
+            Console.WriteLine("indice: " + indice);
+
+            return indice;
+        }
+
+        public int BinarySearch(int[] tableau, int valeur)
+        {
+           Array.Sort(tableau);
+           int g = 0;
+           int d = tableau.Length - 1;
+           int i = 0;
+
+
+       while (i <= d )
+            {
+                i = (g + d) / 2;
+                if (tableau[i] < valeur)
+                    g = i + 1;
+                else
+                    d = i - 1;
+            }
+
+            Console.WriteLine("indice: " + i);
+
+            return i;
+        }
+    }
+}
---
+++ Serie II bis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+
+            // Test exo1 search
+                Search search = new Search();
+                // création du tableau pour le test
+                int[] tab = new int[] {1,-5,10,-3,0,4,2,-7};
+                Console.WriteLine("LinearSearch");
+                search.LinearSearch(tab,-10);
+            // dans le pire des cas, on doit lire i+1 éléments dans le tableau précédent.
+
+            // Test exo1 dichotomique
+            Console.WriteLine("BinarySearch");
+            search.BinarySearch(tab, 3);
+
+        }
+    }
+}

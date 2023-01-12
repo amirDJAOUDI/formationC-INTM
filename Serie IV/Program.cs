511c88ab2034697c321e32a87f028c7676a1e334
amir djaoudi <amirouche.djaoudi@yahoo.com>
2023-01-12 16:03:35 +0100

    Serie IV

---
+++ Serie IV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            // Exercice I: Code Morse
+            Console.WriteLine("Exercice I: Code Morse");
+            Morse codeMorse = new Morse();
+            // exemple AZUL en morse
+            codeMorse.LettersCount("=.===...===.===.=.=...=.=.===...=.===.=.=");
+            // exemple AZUL AMIR en morse
+            codeMorse.WordsCount("=.===...===.===.=.=...=.=.===...=.===.=.=.......=.===...===.===...=.=...=.===.=");
+        }
+    }
+}

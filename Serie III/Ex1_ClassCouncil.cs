95f4180e5ac271d0b1cb0f006c6611703444df85
amir djaoudi <amirouche.djaoudi@yahoo.com>
2023-01-12 14:07:41 +0100

    Serie III

---
+++ Serie III/Ex1_ClassCouncil.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_III
+{ 
+
+    public class ClassCouncil
+    {
+        public void SchoolMeans(string input, string output)
+
+
+
+        {
+            string[] listNotesParNomEtMatiere = input.Split(',');
+            double moyenneFrancais = 0;
+            Boolean premierPassageFrancais = false;
+            double moyenneMaths = 0;
+            Boolean premierPassageMaths = false;
+            double moyenneHistoire = 0;
+            Boolean premierPassageHistoire = false;
+
+            for (int i =0; i < listNotesParNomEtMatiere.Length;i++)
+            {
+
+                switch (listNotesParNomEtMatiere[i])
+                {
+
+                    case "Francais":
+
+                        if (premierPassageFrancais)
+                                {
+                            moyenneFrancais = CalcMoyenne(moyenneFrancais, listNotesParNomEtMatiere[i + 1]);
+
+                        } else
+                        {
+                            moyenneFrancais = Convert.ToDouble(listNotesParNomEtMatiere[i + 1], new CultureInfo("en-US"));
+                            premierPassageFrancais = true;
+                        }
+
+                        break;
+
+                    case "Maths":
+
+                        if (premierPassageMaths)
+                            {
+                            moyenneMaths = CalcMoyenne(moyenneMaths, listNotesParNomEtMatiere[i + 1]);
+
+                        }
+                        else
+                        {
+                            moyenneMaths = Convert.ToDouble(listNotesParNomEtMatiere[i + 1], new CultureInfo("en-US"));
+                            premierPassageMaths = true;
+                        }
+
+                        break;
+
+                    case "Histoire":
+
+                        if (premierPassageHistoire)
+                        {
+                            moyenneHistoire = CalcMoyenne(moyenneHistoire, listNotesParNomEtMatiere[i + 1]);
+                        }
+                        else
+                        {
+                            moyenneHistoire = Convert.ToDouble(listNotesParNomEtMatiere[i + 1], new CultureInfo("en-US"));
+                            premierPassageHistoire = true;
+                        }
+                        break;
+
+
+
+                }
+
+            }
+            Console.WriteLine("La moyenne de Francais: " + moyenneFrancais);
+            Console.WriteLine("La moyenne de Maths: " + moyenneMaths);
+            Console.WriteLine("La moyenne d'Histoire: " + moyenneHistoire);
+        }
+    public double CalcMoyenne(double savMoyenne, string noteMatiere)
+        {
+            double moyennes = (savMoyenne + Convert.ToDouble(noteMatiere, new CultureInfo("en-US"))) / 2;
+
+            return moyennes;
+        }
+    }
+}
---
+++ Serie III/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            // Exercice I: Conseil de classe
+            ClassCouncil conseilClasse = new ClassCouncil();
+            Console.WriteLine("SchoolMeans");
+            conseilClasse.SchoolMeans("Marc,Histoire,12.0,Paul,Histoire,15.5,Yvonne,Maths,6.0,David,Francais,1.2,Paul,Maths,15.5", "Histoire, Maths");
+        }
+    }
+}
